728c997cae943739d3176afdfce1c8cd629642a9
Vasanth Sarathy <Home@vasanths-imac.home>
2014-02-03 10:13:17 -0500

    Allow users to enter data on a form, and hook it all up to the database.

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -6,4 +6,19 @@ class PicsController < ApplicationController
 
 	end
 
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		@pic = Pic.create( pic_params )
+		redirect_to pics_path
+	end
+
+	private 
+
+	def pic_params
+		params.require(:pic).permit(:lesson, :emotion, :learned)
+	end
+
 end
